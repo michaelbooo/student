0125791a08bcbcfcdba2bc23abe15bd29b9f08c3
ts-zhaorigetu.bao <ts-zhaorigetu.bao@mail.rakuten.com>
2015-03-18 14:47:08 +0900

    kick off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Student::Application.config.secret_token = 'ef2d78ae578bc36a141371a45fe388d42339626980688016c0d53e111fa720a5baef7f23a187f4f86a8cd5b004de7a8a27d3635ab0d20d33eadd3befc5cb5738'
